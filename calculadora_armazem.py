8e8f259d03f12c13c75681287b173da819053b05
PKConsultoria <pkconsultoriaitj@gmail.com>
2025-08-18 14:56:10 -0300

    Add files via upload

--- calculadora_armazem.py
+++ calculadora_armazem.py
@@ -307,20 +307,13 @@ with st.expander("📥 Recebimento"):
             # Etiquetagem
             # -----------------------------
             elif "Etiquetagem" in nome:
-            salario_assistente = 3713.31
+                salario_assistente = 3713.31
                 tempo_pallet_h = 1 / 3600  # 1 segundo por pallet
                 demanda_horas = tempo_pallet_h * qtd_containers * qtd_caixas
                 headcount_val = dias_trabalhados * horas_trabalhadas_dia * (eficiencia / 100)
                 taxa_ocupacao = (demanda_horas / headcount_val) if headcount_val else 0
                 # mesmo padrão: salário x taxa de ocupação x demanda
                 custo_item = salario_assistente * taxa_ocupacao * demanda_horas
-
-            elif "Etiquetas" in nome:
-                custo_item = 0.06 * qtd_containers * qtd_caixas
-                tempo_horas = 0
-                demanda_horas = 0
-                headcount_val = ""
-                taxa_ocupacao = 0
 
                 custo_servicos += custo_item
                 discriminacao.append({
